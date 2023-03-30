484f9f333055bb5d3f26d159330a9c5fb8a85f29
Brosa <brunoarosa1997@gmail.com>
2023-03-29 23:42:31 -0400

    fied error on switch function and generated an example file

--- index.js
+++ index.js
@@ -3,33 +3,38 @@ const inquirer = require('inquirer')
 const questions = require('./lib/questions')
 const {Circle, Triangle, Square } = require('./lib/shapes')
 
-function generatelogo(data) {
-    const { text, textColor, shape, shapeColor } = data
+function generatelogo(shapeInfo) {
+    const { text, textColor, shape, shapeColor } = shapeInfo
+    //? Help me understand switch!!
+    let shapeRendered;
     switch (shape) {
         case "Circle":
             const circleSelected = new Circle(text, textColor, shape, shapeColor)
-            circleSelected.render()
+            shapeRendered = circleSelected.render()
             break;
         case "Triangle":
             const triangleSelected = new Triangle(text, textColor, shape, shapeColor)
-            triangleSelected.render()
+            shapeRendered = triangleSelected.render()
             break;
         case "Square":
             const squareSelected = new Square(text, textColor, shape, shapeColor)
-            squareSelected.render()
+            shapeRendered = squareSelected.render()
             break;
+        //? Do I need a deafult when I have a list?
         default:
             console.log('An error has occured. Please select a shape')
-
     }
+    //? Is this where I should console log?
+    console.log('Generated logo.svg')
+    return shapeRendered
 }
 
 function init() {
     inquirer.prompt(questions)
         .then((answers) => {
-            fs.writeFile('./examples/logo.svg', generatelogo(answers), (error) => console.log(error))
+            fs.writeFile('./examples/logo.svg', generatelogo(answers), err => console.log(err))
         })
-        .catch((error) => console.log(error))
+        .catch((err) => console.log(err))
 }
 
 init()
