0e089580e4159bcf0e763107ad45e577324d8199
Brosa <brunoarosa1997@gmail.com>
2023-03-29 22:24:51 -0400

    created children for shape and added a switch function to handle user shape selected

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
 const questions = require('./lib/questions')
-const Shape = require('./lib/shapes')
+const {Circle, Triangle, Square } = require('./lib/shapes')
 
-function generatelogo(data){
-    const {text,textColor,shape,shapeColor} = data
-    const userShape = new Shape(text,textColor,shape,shapeColor)
-    console.log(userShape)
+function generatelogo(data) {
+    const { text, textColor, shape, shapeColor } = data
+    switch (shape) {
+        case "Circle":
+            const circleSelected = new Circle(text, textColor, shape, shapeColor)
+            circleSelected.render()
+            break;
+        case "Triangle":
+            const triangleSelected = new Triangle(text, textColor, shape, shapeColor)
+            triangleSelected.render()
+            break;
+        case "Square":
+            const squareSelected = new Square(text, textColor, shape, shapeColor)
+            squareSelected.render()
+            break;
+        default:
+            console.log('An error has occured. Please select a shape')
+
+    }
 }
 
-function init(){
+function init() {
     inquirer.prompt(questions)
-    .then((answers) => {
-        fs.writeFile('./examples/logo.svg', generatelogo(answers), (error) => console.log(error))
-    })
-    .catch((error) => console.log(error))
+        .then((answers) => {
+            fs.writeFile('./examples/logo.svg', generatelogo(answers), (error) => console.log(error))
+        })
+        .catch((error) => console.log(error))
 }
 
 init()
