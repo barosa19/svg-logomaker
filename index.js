c449e13a8b38b3981a3f5dab441f71a308e1fcd8
Brosa <brunoarosa1997@gmail.com>
2023-03-29 22:09:18 -0400

    created a class Shape

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
 const questions = require('./lib/questions')
+const Shape = require('./lib/shapes')
+
+function generatelogo(data){
+    const {text,textColor,shape,shapeColor} = data
+    const userShape = new Shape(text,textColor,shape,shapeColor)
+    console.log(userShape)
+}
 
 function init(){
     inquirer.prompt(questions)
     .then((answers) => {
-        console.log(answers)
+        fs.writeFile('./examples/logo.svg', generatelogo(answers), (error) => console.log(error))
     })
     .catch((error) => console.log(error))
 }
